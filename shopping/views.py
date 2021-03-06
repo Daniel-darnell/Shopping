f7e73ed7293ed79a4f3e5dd6bb3d269c43307c55
Daniel-darnell <darnelldanny1997@gmail.com>
2021-03-07 01:15:38 +0300

    Added Shopping item viewset

--- shopping/views.py
+++ shopping/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import Viewsets
+from .models import ShoppingItem
+from .serializers import ShoppingItemSerializer
 
 # Create your views here.
+class ShoppingItemViewSet(viewsets.ModelViewSet):
+    serializer_class = ShoppingItemSerializer
+    queryset = ShoppingItem.objects.all()
+    
